fa829c01986573ff6f16d0065d8a42a6a13876aa
ByeoungJoon Jeon <juni8453@naver.com>
2022-12-20 18:57:16 +0900

    :tada: Begin project
    ## Description
    - 프로젝트 시작 !

---
+++ src/test/java/com/market/carrot/CarrotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.market.carrot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarrotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
