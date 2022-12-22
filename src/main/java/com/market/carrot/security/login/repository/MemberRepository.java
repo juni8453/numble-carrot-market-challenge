6df6b2b13cac800b520554156059d812cb434007
ByeoungJoon Jeon <juni8453@naver.com>
2022-12-23 00:19:16 +0900

    :fire: 이전 폼 로그인 구현 파일 삭제
    ## Description
    - 이전에 폼 로그인을 구현했던 설정, 구현 파일 모두 삭제
    - 새로 구현 예정

--- src/main/java/com/market/carrot/security/login/repository/MemberRepository.java
+++
@@ -1,9 +0,0 @@
-package com.market.carrot.security.login.repository;
-
-import com.market.carrot.security.login.domain.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<Member, Long> {
-
-  Member findByUsername(String username);
-}
