09eb30733abb5fd89503b80d8fae984d755ac829
ByeoungJoon Jeon <juni8453@naver.com>
2022-12-26 10:16:18 +0900

    :wrench: Security Config 수정
    ## Description
    - OAuth 로그인 인증로직 추가
    - 후처리 클래스 OAuth2UserDetailsService 추가

--- src/main/java/com/market/carrot/login/config/SecurityConfig.java
+++ src/main/java/com/market/carrot/login/config/SecurityConfig.java
@@ -1,9 +1,11 @@
 package com.market.carrot.login.config;
 
+import com.market.carrot.login.config.customAuthentication.CustomOAuth2UserDetailsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
@@ -14,7 +16,10 @@ import org.springframework.security.web.SecurityFilterChain;
 @RequiredArgsConstructor
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true)
 public class SecurityConfig {
+
+  private final CustomOAuth2UserDetailsService oAuth2UserDetailsService;
 
   @Bean
   public PasswordEncoder passwordEncoder() {
@@ -36,10 +41,23 @@ public class SecurityConfig {
         .antMatchers("/admin/**").hasRole("ADMIN")
         .anyRequest().permitAll();
 
+    // Form Login 인증 설정
     http.formLogin()
         .loginPage("/loginForm")
         .loginProcessingUrl("/login") // /login 추소 호출 시 시큐리티카 낚아채서 대신 로그인 진행 (컨트롤러 만들 필요 X)
-        .defaultSuccessUrl("/"); // 로그인 성공 후 보내지는 주소
+        .defaultSuccessUrl("/"); // 로그인 성공 후 보내지는 주소 (특정 페이지 요청해서 인증이 성공되면 / 가 아닌 요청한 페이지로 자동 이동)
+
+    // OAuth2 Login 인증 설정
+    http.oauth2Login()
+        .loginPage("/loginForm")
+
+        /**
+         * OAuth 로그인 후 후처리가 필요.
+         * 1. Access Token + 사용자 프로필 정보 가져오기
+         * 2. OAuth 에서 넘긴 프로필 정보 이외의 추가적인 정보가 필요없다면 자동으로 회원가입 진행시키면 됨.
+         */
+        .userInfoEndpoint()
+        .userService(oAuth2UserDetailsService);
 
     return http.build();
   }
