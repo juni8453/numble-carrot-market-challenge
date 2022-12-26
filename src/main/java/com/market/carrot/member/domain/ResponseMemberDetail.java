c14b32714c86afb66262ff077db4c76b14914947
ByeoungJoon Jeon <juni8453@naver.com>
2022-12-26 18:31:59 +0900

    :sparkles: Global Response 객체 추가
    ## Description
    1. GlobalResponseDto
      - API 명세를 지키기 위한 공통 Response 객체
      - code : 1(성공), -1(실패) 로 구분
      - message : 성공 시 메세지
      - body : Response Body 데이터
    
    2. ResponseMemberDetail
      - 회원 조회 시 클라이언트로 내려주는 응답 객체
      - @Builder 패턴 사용

---
+++ src/main/java/com/market/carrot/member/domain/ResponseMemberDetail.java
@@ -0,0 +1,37 @@
+package com.market.carrot.member.domain;
+
+import com.market.carrot.login.domain.Member;
+import com.market.carrot.login.domain.Role;
+import java.time.LocalDateTime;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class ResponseMemberDetail {
+
+  private final String username;
+  private final String email;
+  private final Role role;
+  private final LocalDateTime createdDate;
+  private final LocalDateTime modifiedDate;
+
+  @Builder
+  private ResponseMemberDetail(String username, String email, Role role,
+      LocalDateTime createdDate, LocalDateTime modifiedDate) {
+    this.username = username;
+    this.email = email;
+    this.role = role;
+    this.createdDate = createdDate;
+    this.modifiedDate = modifiedDate;
+  }
+
+  public static ResponseMemberDetail of(Member member) {
+    return ResponseMemberDetail.builder()
+        .username(member.getUsername())
+        .email(member.getEmail())
+        .role(member.getRole())
+        .createdDate(member.getCreatedDate())
+        .modifiedDate(member.getModifiedDate())
+        .build();
+  }
+}
