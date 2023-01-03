47a7db1d6d8c053bff0ae2934f98310a8b96f022
ByeoungJoon Jeon <juni8453@naver.com>
2023-01-03 20:34:15 +0900

    :sparkles: Form 로그인 로직 수정
    ## Description
    1. LoginServiceImpl
      - 중복되는 아이디는 회원가입 시 UserDuplicatedException 발생
    
    2. UserDuplicatedException
      - 커스텀 예외 클래스 생성
    
    3. GlobalExceptionHandler
      - 커스텀 예외 클래스 처리 Handler 추가

---
+++ src/main/java/com/market/carrot/global/Exception/UserDuplicatedException.java
@@ -0,0 +1,16 @@
+package com.market.carrot.global.Exception;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@RequiredArgsConstructor
+public class UserDuplicatedException extends RuntimeException {
+
+  private final String errorMessage;
+  private final HttpStatus httpStatus;
+
+  @Override
+  public String getMessage() {
+    return errorMessage;
+  }
+}
--- src/main/java/com/market/carrot/login/config/customAuthentication/form/CustomUserDetailsService.java
+++ src/main/java/com/market/carrot/login/config/customAuthentication/form/CustomUserDetailsService.java
@@ -1,9 +1,11 @@
 package com.market.carrot.login.config.customAuthentication.form;
 
+import com.market.carrot.global.Exception.NotFoundEntityException;
 import com.market.carrot.login.config.customAuthentication.common.MemberContext;
 import com.market.carrot.login.domain.LoginRepository;
 import com.market.carrot.login.domain.Member;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -21,7 +23,7 @@ public class CustomUserDetailsService implements UserDetailsService {
      * Provider 로 부터 호출받아 Authentication 내부를 채우고 다시 Provider 로 반환한다.
      */
     Member findMember = loginRepository.findByUsername(username)
-        .orElseThrow(() -> new UsernameNotFoundException("찾을 수 없는 멤버입니다."));
+        .orElseThrow(() -> new NotFoundEntityException("찾을 수 없는 멤버입니다.", HttpStatus.BAD_REQUEST));
 
     return new MemberContext(findMember);
   }
