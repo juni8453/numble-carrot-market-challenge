8ccc0d635ce557ee297ac2fbe4703cce052cbc40
ByeoungJoon Jeon <juni8453@naver.com>
2023-01-10 20:15:52 +0900

    :sparkles: Product MVC 클래스, 인터페이스 생성
    ## Description
    1. ProductController
    
    2. ProductRepository
    
    3. ProductService
    
    4. ProductServiceImpl

---
+++ src/main/java/com/market/carrot/product/service/ProductServiceImpl.java
@@ -0,0 +1,38 @@
+package com.market.carrot.product.service;
+
+import com.market.carrot.product.domain.Product;
+import com.market.carrot.product.domain.ProductRepository;
+import com.market.carrot.product.dto.response.CategoryByProductResponse;
+import com.market.carrot.product.dto.response.ImagesResponse;
+import com.market.carrot.product.dto.response.MemberByProductResponse;
+import com.market.carrot.product.dto.response.ProductResponse;
+import java.util.List;
+import java.util.stream.Collectors;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class ProductServiceImpl implements ProductService {
+
+  private final ProductRepository productRepository;
+
+  @Transactional(readOnly = true)
+  @Override
+  public List<ProductResponse> readAll() {
+
+    return productRepository.readAll().stream()
+        .map(product -> ProductResponse.builder()
+            .id(product.getId())
+            .title(product.getTitle())
+            .content(product.getContent())
+            .price(product.getPrice())
+            .heartCount(product.getHeartCount())
+            .member(MemberByProductResponse.from(product))
+            .category(CategoryByProductResponse.from(product))
+            .images(ImagesResponse.from(product))
+            .build())
+        .collect(Collectors.toList());
+  }
+}
