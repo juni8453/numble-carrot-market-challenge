40a7c94473ab71f711b9691b2881bc3010f9c550
ByeoungJoon Jeon <juni8453@naver.com>
2023-01-12 13:13:32 +0900

    :sparkles: Product 관련 수정사항
    ## Description
    1. Product
      - BaseTime 추상 클래스 상속받도록 수정
    2. ProductApiController
      - @Slf4j 삭제 (일단 안쓰는 거라 ..)
    3. ProductResponse
      - 전체 Product 조회 시 Created, ModifiedDate 반환하도록 필드 및 생성자 인자로 추가
    4. ProductServiceImpl
      - DB 조회 결과로 받은 List<Product> 타입의 created, modifiedDate 를 List<ProductResponse> 타입으로 반환할 수 있도록 Stream API map() 변환 로직에 추가

--- src/main/java/com/market/carrot/product/controller/ProductApiController.java
+++ src/main/java/com/market/carrot/product/controller/ProductApiController.java
@@ -5,16 +5,14 @@ import com.market.carrot.product.dto.response.ProductResponse;
 import com.market.carrot.product.service.ProductService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Slf4j
 @RequiredArgsConstructor
 @RequestMapping("/api")
 @RestController
-public class ProductController {
+public class ProductApiController {
 
   private final ProductService productService;
 
