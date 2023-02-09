0044943d9a4fa8ad965ca7c1ec927f2e490ef7de
ByeoungJoon Jeon <juni8453@naver.com>
2023-02-09 20:01:38 +0900

    :bulb: GlobalResponseDto 필드 변수 추가
    ## Description
    1. GlobalResponseDto
      - HttpStatus 추가

--- src/main/java/com/market/carrot/global/GlobalResponseDto.java
+++ src/main/java/com/market/carrot/global/GlobalResponseDto.java
@@ -2,23 +2,21 @@ package com.market.carrot.global;
 
 import lombok.Builder;
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
 
 @Getter
 public class GlobalResponseDto {
 
   private final Integer code;
+  private final HttpStatus httpStatus;
   private final String message;
   private final Object body;
 
   @Builder
-  public GlobalResponseDto(Integer code, String message, Object body) {
+  public GlobalResponseDto(Integer code, HttpStatus httpStatus, String message, Object body) {
     this.code = code;
+    this.httpStatus = httpStatus;
     this.message = message;
     this.body = body;
-  }
-
-  @Builder
-  public GlobalResponseDto(Integer code, String message) {
-    this(code, message, null);
   }
 }
