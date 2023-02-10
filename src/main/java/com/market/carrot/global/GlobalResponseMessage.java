cb60a4f6739e64d7b72b8610371861172493900c
ByeoungJoon Jeon <juni8453@naver.com>
2023-02-10 17:52:09 +0900

    :sparkles: 응답 메세지 Enum 클래스화
    ## Description
    - 모든 성공 메세지를 관리하도록 Enum 클래스 생성

---
+++ src/main/java/com/market/carrot/global/GlobalResponseMessage.java
@@ -0,0 +1,31 @@
+package com.market.carrot.global;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum GlobalResponseMessage {
+
+  SUCCESS_PRODUCT_LIKE("제품 좋아요 성공"),
+
+  SUCCESS_GET_MEMBER("단건 회원 조회 성공"),
+
+  SUCCESS_DELETE_MEMBER("단건 회원 삭제 성공"),
+
+  SUCCESS_GET_PRODUCTS("모든 상품 조회 성공"),
+
+  SUCCESS_GET_PRODUCT("단건 상품 조회 성공"),
+
+  SUCCESS_POST_INSERT_PRODUCT("상품 등록 성공"),
+
+  SUCCESS_POST_UPDATE_PRODUCT("상품 수정 성공"),
+
+  SUCCESS_DELETE_PRODUCT("상품 삭제 성공"),
+
+  SUCCESS_POST_UPDATE_IMAGE("이미지 수정 성공"),
+
+  SUCCESS_DELETE_IMAGE("이미지 삭제 성공");
+
+  private final String successMessage;
+}
