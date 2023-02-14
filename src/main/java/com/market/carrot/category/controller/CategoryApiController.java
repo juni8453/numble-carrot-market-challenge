14a8cd6c3c904ff45bbfbd2149464ebc274a6617
ByeoungJoon Jeon <juni8453@naver.com>
2023-02-14 17:27:16 +0900

    :bulb: Category 관련 수정
    ## Description
    1. CategoryApiController
      - 카테고리 생성을 위한 Controller 생성
    
    2. CategoryService
      - 카테고리는 ADMIN 권한 유저만 등록할 수 있으므로, save() 매개변수로 MemberContext 추가
    
    3. GlobalResponseMessage
      - 카테고리 등록에 사용할 알맞은 Success 메시지 생성
      - 제품 좋아요 성공 네이밍 수정

---
+++ src/main/java/com/market/carrot/category/controller/CategoryApiController.java
@@ -0,0 +1,38 @@
+package com.market.carrot.category.controller;
+
+import com.market.carrot.category.domain.dto.CreateCategoryRequest;
+import com.market.carrot.category.service.CategoryService;
+import com.market.carrot.global.GlobalResponseDto;
+import com.market.carrot.global.GlobalResponseMessage;
+import com.market.carrot.login.config.customAuthentication.common.MemberContext;
+import lombok.RequiredArgsConstructor;
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RequestMapping(value = "/api/category/", produces = MediaTypes.HAL_JSON_VALUE)
+@RestController
+public class CategoryApiController {
+
+  private final CategoryService categoryService;
+
+  @ResponseStatus(HttpStatus.CREATED)
+  @PostMapping
+  GlobalResponseDto save(@RequestBody CreateCategoryRequest createCategoryRequest,
+      @AuthenticationPrincipal MemberContext memberContext) {
+
+    categoryService.save(createCategoryRequest, memberContext);
+
+    return GlobalResponseDto.builder()
+        .code(1)
+        .httpStatus(HttpStatus.CREATED)
+        .message(GlobalResponseMessage.SUCCESS_POST_CATEGORY.getSuccessMessage())
+        .build();
+  }
+}
