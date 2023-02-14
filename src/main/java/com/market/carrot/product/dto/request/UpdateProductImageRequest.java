ec3dab41ace1133b41e0efe2daefb90b35cdcebd
ByeoungJoon Jeon <juni8453@naver.com>
2023-02-14 15:21:03 +0900

    :bulb: UpdateProductImageRequest 수정
    ## Description
    - 생성자 추가 및 팩토리 메서드 타입 생성자 추가
    - 생성자 추가로 인해 역직렬화를 위해 @NoArgsConstructor 어노테이션 추가

--- src/main/java/com/market/carrot/product/dto/request/UpdateProductImageRequest.java
+++ src/main/java/com/market/carrot/product/dto/request/UpdateProductImageRequest.java
@@ -1,8 +1,20 @@
 package com.market.carrot.product.dto.request;
 
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 public class UpdateProductImageRequest {
+
   private String imageUrl;
+
+  private UpdateProductImageRequest(String imageUrl) {
+    this.imageUrl = imageUrl;
+  }
+
+  public static UpdateProductImageRequest testConstructor(String imageUrl) {
+    return new UpdateProductImageRequest(imageUrl);
+  }
 }
