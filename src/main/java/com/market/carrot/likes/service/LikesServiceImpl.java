ae4908f3127eae865fe7b85994047d6b57e992bd
ByeoungJoon Jeon <juni8453@naver.com>
2023-02-22 16:10:31 +0900

    :bulb: 사용하지 않는 Import 문 삭제
    ## description
    - 사용하지 않는 Import 문 삭제

--- src/main/java/com/market/carrot/likes/service/LikesServiceImpl.java
+++ src/main/java/com/market/carrot/likes/service/LikesServiceImpl.java
@@ -5,7 +5,6 @@ import com.market.carrot.global.Exception.NotFoundEntityException;
 import com.market.carrot.likes.domain.Likes;
 import com.market.carrot.likes.domain.LikesRepository;
 import com.market.carrot.login.config.customAuthentication.common.MemberContext;
-import com.market.carrot.member.domain.MemberRepository;
 import com.market.carrot.product.domain.Product;
 import com.market.carrot.product.domain.ProductRepository;
 import lombok.RequiredArgsConstructor;
@@ -19,15 +18,16 @@ public class LikesServiceImpl implements LikesService {
 
   private final LikesRepository likesRepository;
   private final ProductRepository productRepository;
-  private final MemberRepository memberRepository;
 
   @Transactional
   @Override
   public void like(Long productId, MemberContext memberContext) {
     Product findProduct = productRepository.findById(productId)
-        .orElseThrow(() -> new NotFoundEntityException(ExceptionMessage.NOT_FOUND_PRODUCT, HttpStatus.BAD_REQUEST));
+        .orElseThrow(() -> new NotFoundEntityException(ExceptionMessage.NOT_FOUND_PRODUCT,
+            HttpStatus.BAD_REQUEST));
 
-    Likes findLikes = likesRepository.findByUsernameAndProductId(memberContext.getUsername(), productId);
+    Likes findLikes = likesRepository.findByUsernameAndProductId(memberContext.getUsername(),
+        productId);
 
     // 자신이 좋아요를 이미 누른 제품인 경우 heartCount 를 하나 줄이고 Entity 를 DB 에서 삭제한다.
     if (findLikes != null) {
