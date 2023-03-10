fdf1f6af4da2650d7e4a0fc65c82eaa548b78707
ByeoungJoon Jeon <juni8453@naver.com>
2023-03-10 12:59:58 +0900

    :ambulance: 좋아요 증감 동시성 제어
    ## description
    - Pessimistic Lock 을 활용해 동시성 제어할 수 있도록 수정

--- src/main/java/com/market/carrot/product/domain/ProductRepository.java
+++ src/main/java/com/market/carrot/product/domain/ProductRepository.java
@@ -2,7 +2,9 @@ package com.market.carrot.product.domain;
 
 import java.util.List;
 import java.util.Optional;
+import javax.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -11,6 +13,7 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
   @Query("SELECT p FROM Product p JOIN FETCH p.member JOIN FETCH p.category")
   List<Product> readAll();
 
+  @Lock(value = LockModeType.PESSIMISTIC_WRITE)
   @Query("SELECT p FROM Product p JOIN FETCH p.member JOIN FETCH p.category WHERE p.id = :id")
-  Optional<Product> findById(@Param("id") Long id);
+  Optional<Product> findByIdWithPessimisticLock(@Param("id") Long id);
 }
