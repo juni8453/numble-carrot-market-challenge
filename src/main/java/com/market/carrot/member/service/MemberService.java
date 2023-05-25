b89fa1e17d6a355959813675e3368dc404716747
ByeoungJoon Jeon <juni8453@naver.com>
2023-05-25 19:19:00 +0900

    :fire: MemberModel 삭제
    ## description
    - Hateos 제거로 인해 MemberModel 클래스 삭제
    - MemberService 의 import 삭제

--- src/main/java/com/market/carrot/member/service/MemberService.java
+++ src/main/java/com/market/carrot/member/service/MemberService.java
@@ -2,7 +2,6 @@ package com.market.carrot.member.service;
 
 import com.market.carrot.login.config.customAuthentication.common.MemberContext;
 import com.market.carrot.member.controller.dto.response.MemberResponse;
-import com.market.carrot.member.hateoas.MemberModel;
 
 public interface MemberService {
 
